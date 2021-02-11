5423a8777b0bba1a00f32c001b83ed38c4d78153
infotechbro <77918121+infotechbro@users.noreply.github.com>
2021-02-11 15:29:14 +0530

    Update chat_status.py

--- tg_bot/modules/helper_funcs/chat_status.py
+++ tg_bot/modules/helper_funcs/chat_status.py
@@ -245,9 +245,9 @@ def can_promote(func):
         message_chat_title = update.effective_message.chat.title
 
         if update_chat_title == message_chat_title:
-            cant_promote = f"I can't promote/demote people here!\n {mention} Make sure I'm admin and can appoint new admins."
+            cant_promote = f" {mention} I can't promote/demote people here!\n {mention} Make sure I'm admin and can appoint new admins."
         else:
-            cant_promote = (f"I can't promote/demote people in <b>{update_chat_title}</b>!\n"
+            cant_promote = (f"{mention} I can't promote/demote people in <b>{update_chat_title}</b>!\n"
                             f"{mention} Make sure I'm admin there and can appoint new admins.")
 
         if chat.get_member(bot.id).can_promote_members:
