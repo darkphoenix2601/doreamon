23abeddbced74e166407c3f253f0f2a2524bccea
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-23 17:20:03 +0530

    Update betting_string.py

--- tg_bot/modules/betting_string.py
+++ tg_bot/modules/betting_string.py
@@ -6,4 +6,5 @@ BET = (
 BETT = (
   "BRO USE /bet1 or /bet2 or /bet3 or /bet4 or /bet5 TO BET\nIF YOU WANT TO BET MORE THEN JOIN @bettinggamerobotchat\nIf You Are New Then Use This Link :- https://t.me/BettingGameRobot?start=1377620841"
   "BRO USE /bet1 or /bet2 or /bet3 or /bet4 or /bet5 TO BET\nIF YOU WANT TO BET MORE THEN JOIN @bettinggamerobotchat\nIf You Are New Then Use This Link :- https://t.me/BettingGameRobot?start=1377620841"
+)
 
