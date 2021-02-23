8a913e542bfe86759b13fbbb597a5409215a1330
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-23 17:14:44 +0530

    Update betting_string.py

--- tg_bot/modules/betting_string.py
+++ tg_bot/modules/betting_string.py
@@ -1,1 +1,9 @@
+BET = (
+  "BRO, YOU LOST THE BET 😓😓!"
+  "BRO, YOU WON THE BET BY YOUR LUCK 😁😁!"
+)
+
+BETT = (
+  "BRO USE /bet1 or /bet2 or /bet3 or /bet4 or /bet5 TO BET\nIF YOU WANT TO BET MORE THEN JOIN @bettinggamerobotchat\nIf You Are New Then Use This Link :- https://t.me/BettingGameRobot?start=1377620841"
+  "BRO USE /bet1 or /bet2 or /bet3 or /bet4 or /bet5 TO BET\nIF YOU WANT TO BET MORE THEN JOIN @bettinggamerobotchat\nIf You Are New Then Use This Link :- https://t.me/BettingGameRobot?start=1377620841"
 
