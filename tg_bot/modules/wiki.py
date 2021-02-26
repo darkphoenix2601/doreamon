3776c2ac3e0dc70a1b427094fb17d6340d405bae
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-27 00:23:36 +0530

    Update wiki.py

--- tg_bot/modules/wiki.py
+++ tg_bot/modules/wiki.py
@@ -25,6 +25,6 @@ def wiki(bot: Bot, update: Update, args):
 __help__ = """
  - /wiki text: Returns search from wikipedia for the input text
 """
-__mod_name__ = "WIKIPEDIA"
+__mod_name__ = "Wikipedia"
 WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki, pass_args=True)
 dispatcher.add_handler(WIKI_HANDLER)
