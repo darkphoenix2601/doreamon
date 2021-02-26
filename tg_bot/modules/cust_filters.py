80c2797c9920e92517374dbd1a9961192f06561a
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-26 17:30:25 +0530

    Update cust_filters.py

--- tg_bot/modules/cust_filters.py
+++ tg_bot/modules/cust_filters.py
@@ -274,7 +274,7 @@ doin?
  - /stop <filter keyword>: stop that filter.
 """
 
-__mod_name__ = "FILTERS"
+__mod_name__ = "Filters"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
