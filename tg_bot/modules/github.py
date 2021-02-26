fe93d5109d5b4d4863d226eb0446bcc9f276524a
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-26 23:58:03 +0530

    Update github.py

--- tg_bot/modules/github.py
+++ tg_bot/modules/github.py
@@ -14,21 +14,21 @@ def github(bot: Bot, update: Update):
     usr = get(f'https://api.github.com/users/{text}').json()
     if usr.get('login'):
         reply_text = f"""*Name:* `{usr['name']}`
-*Username:* `{usr['login']}`
-*Account ID:* `{usr['id']}`
-*Account type:* `{usr['type']}`
-*Location:* `{usr['location']}`
-*Bio:* `{usr['bio']}`
-*Followers:* `{usr['followers']}`
-*Following:* `{usr['following']}`
-*Hireable:* `{usr['hireable']}`
-*Public Repos:* `{usr['public_repos']}`
-*Public Gists:* `{usr['public_gists']}`
-*Email:* `{usr['email']}`
-*Company:* `{usr['company']}`
-*Website:* `{usr['blog']}`
-*Last updated:* `{usr['updated_at']}`
-*Account created at:* `{usr['created_at']}`
+*🔥 Username* ☞︎︎︎ `{usr['login']}`
+*🆔 Account ID* ☞︎︎︎ `{usr['id']}`
+*💠 Account Type* ☞︎︎︎ `{usr['type']}`
+*📍 Location* ☞︎︎︎ `{usr['location']}`
+*☣️ Bio* ☞︎︎︎ `{usr['bio']}`
+*🧑‍🤝‍🧑 Followers* ☞︎︎︎ `{usr['followers']}`
+*🤴 Following* ☞︎︎︎ `{usr['following']}`
+*🌊 Hireable* ☞︎︎︎ `{usr['hireable']}`
+*👫 Public Repos* ☞︎︎︎ `{usr['public_repos']}`
+*👬 Public Gits* ☞︎︎︎ `{usr['public_gists']}`
+*📨 Email* ☞︎︎︎ `{usr['email']}`
+*🔰 Company* ☞︎︎︎ `{usr['company']}`
+*🌐 Website* ☞︎︎︎ `{usr['blog']}`
+*👀 Last updated* ☞︎︎︎ `{usr['updated_at']}`
+*🦠 Account created at* ☞︎︎︎ `{usr['created_at']}`
 """
     else:
         reply_text = "User not found. Make sure you entered valid username!"
@@ -39,7 +39,7 @@ __help__ = """
  - /git:{GitHub username} Returns info about a GitHub user or organization.
 """
 
-__mod_name__ = "GITHUB"
+__mod_name__ = "Github"
 
 github_handle = DisableAbleCommandHandler("git", github)
 
