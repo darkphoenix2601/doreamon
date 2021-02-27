12cb688e6cccef5e4e2466f5b610f4d15a426487
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-27 16:50:44 +0530

    Update cleaner.py

--- tg_bot/modules/cleaner.py
+++ tg_bot/modules/cleaner.py
@@ -197,14 +197,14 @@ def bluetext_ignore_list(bot: Bot, update: Update):
 
 
 __help__ = """
- - /cleanblue <on/off/yes/no> - clean commands after sending
- - /ignoreblue <word> - prevent auto cleaning of the command
- - /unignoreblue <word> - remove prevent auto cleaning of the command
- - /listblue - list currently whitelisted commands
+ ➥ /cleanblue <on/off/yes/no> - clean commands after sending
+ ➥ /ignoreblue <word> - prevent auto cleaning of the command
+ ➥ /unignoreblue <word> - remove prevent auto cleaning of the command
+ ➥ /listblue - list currently whitelisted commands
  
  Following are Disasters only commands, admins cannot use these:
-  - /gignoreblue <word> - globally ignore bluetext cleaning.
- - /ungignoreblue <word> - remove said command from global cleaning list
+ ➥ /gignoreblue <word> - globally ignore bluetext cleaning.
+ ➥ /ungignoreblue <word> - remove said command from global cleaning list
 """
 
 SET_CLEAN_BLUE_TEXT_HANDLER = CommandHandler("cleanblue", set_blue_text_must_click, pass_args=True)
