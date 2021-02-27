42c166b0e29db367b4c70dce390aa29ed066582e
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-02-27 18:12:34 +0530

    Update covidindia.py

--- tg_bot/modules/covidindia.py
+++ tg_bot/modules/covidindia.py
@@ -24,10 +24,20 @@ def covidi(bot: Bot, update: Update):
 def covidc(bot: Bot, update: Update):
   update.effective_message.reply_text(
       "*🦠 COVID-19 Stats In China 🦠:*\n\n"
-        "➥ *Total Confirmed* \nㅤㅤ╚» `" + str(JHU.china.confirmed) + "`\n"
+        "➥ *Total Confirmed* \nㅤㅤ╚» `" + str(JHU.China.confirmed) + "`\n"
         "➥ *Total Deaths* \nㅤㅤ╚» `" + str(JHU.China.deaths) + "`\n"
         "➥ *Total Recovered* \nㅤㅤ╚» `" + str(JHU.China.recovered) +"`\n"
         "➥ *Active Cases* \nㅤㅤ╚» `"+ str(JHU.China.cases) + "`\n\n"
+        "➥ *Tips*\n☞ 😷 Wear A Mask.\n ☞ 🧻 Use Tissue When Sneezing Or Blowing Nose.\n☞ 🧼 Wash Your Hands Frequently.\n☞︎︎︎ 👬 Avoid Contact With Others.\n☞︎︎︎ 🍎 Wash Foods Before Eating It.\n☞︎︎︎ 🛀 Maintain Good Hygiene", parse_mode=ParseMode.MARKDOWN)
+  
+@run_async
+def covidp(bot: Bot, update: Update):
+  update.effective_message.reply_text(
+      "*🦠 COVID-19 Stats In Pakistan 🦠:*\n\n"
+        "➥ *Total Confirmed* \nㅤㅤ╚» `" + str(JHU.Pakistan.confirmed) + "`\n"
+        "➥ *Total Deaths* \nㅤㅤ╚» `" + str(JHU.Pakistan.deaths) + "`\n"
+        "➥ *Total Recovered* \nㅤㅤ╚» `" + str(JHU.Pakistan.recovered) +"`\n"
+        "➥ *Active Cases* \nㅤㅤ╚» `"+ str(JHU.Pakistan.cases) + "`\n\n"
         "➥ *Tips*\n☞ 😷 Wear A Mask.\n ☞ 🧻 Use Tissue When Sneezing Or Blowing Nose.\n☞ 🧼 Wash Your Hands Frequently.\n☞︎︎︎ 👬 Avoid Contact With Others.\n☞︎︎︎ 🍎 Wash Foods Before Eating It.\n☞︎︎︎ 🛀 Maintain Good Hygiene", parse_mode=ParseMode.MARKDOWN)
   
   
@@ -37,6 +47,7 @@ __help__ = """
  
  ➥ /covindia - Get Corona Status Of India
  ➥ /covchina - Get Corona Status Of China
+ ➥ /covpakistan - Get Corona Status Of Pakistan
  
 """
 
@@ -44,9 +55,11 @@ __mod_name__ = 'Covid Tracker'
 
 COVIDI_HANDLER = CommandHandler("covindia", covidi, admin_ok=True)
 COVIDC_HANDLER = CommandHandler("covchina", covidc, admin_ok=True)
+COVIDP_HANDLER = CommandHandler("covipakistan", covidp, admin_ok=True)
 
 
 dispatcher.add_handler(COVIDI_HANDLER)
 dispatcher.add_handler(COVIDC_HANDLER)
+dispatcher.add_handler(COVIDP_HANDLER)
 
 
