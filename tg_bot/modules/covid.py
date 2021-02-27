eb1d2f5eb89f4e64efdc5493b15d44f5b5717ab0
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-27 14:57:15 +0530

    Update covid.py

--- tg_bot/modules/covid.py
+++ tg_bot/modules/covid.py
@@ -1,14 +1,26 @@
-from telegram import ParseMode, Update, Bot, Chat
+from telegram import ParseMode, Update, Bot, Chat, MessageEntity, Filters
 from telegram.ext import CommandHandler, MessageHandler, BaseFilter, run_async
 
 from tg_bot import dispatcher
-
 from covid19_data import JHU
 
 from requests import get
+from tg_bot.modules.disable import DisableAbleCommandHandler
 
 import json
 from urllib.request import urlopen
+
+SFW_STRINGS = (
+    
+    "🧼 WASH YOUR HANDS FREQUENTLY",
+    "🚴‍ ♂️EXCERCISE AND PROPER SLEEP🛌 WILL BOLSTER THE IMMUNE SYSTEM",
+    "🛀 MAINTAIN GOOD HYGIENE HABHITS AT ALL TIMES",
+    "👬 AVOID CONTACT WITH OTHERS",
+    "😷 WEAR A FACE MASK WHEN DEALING WITH INFECTED PATIENT'S",
+    "🧻 USE TISSUES WHEN COUGHING OR BLOWING NOSE",
+    "🍎 WASH AND PREPARE FOODS CAREFULLY",
+    "STAY HOME STAY SAFE",
+  )
 
 
 
@@ -20,15 +32,34 @@ def covid(bot: Bot, update: Update):
         "➥ *Total Deaths* \nㅤㅤ╚» `" + str(JHU.Total.deaths) + "`\n"
         "➥ *Total Recovered* \nㅤㅤ╚» `" + str(JHU.Total.recovered) +"`\n"
         "➥ *Active Cases* \nㅤㅤ╚» `"+ str(JHU.India.cases) + "`\n\n"
-        "➥ *Tips*\n☞ 😷 Wear A Mask.\n☞ 🧻 Use Tissue When Sneezing Or Blowing Nose.\n☞ 🧼 Wash Your Hands Frequently.\n☞︎︎︎ 👬 Avoid Contact With Others.\n☞︎︎︎ 🍎 Wash Foods After buying it.", parse_mode=ParseMode.MARKDOWN)
+        "➥ *Tips*\n☞ 😷 Wear A Mask.\n
+        ☞ 🧻 Use Tissue When Sneezing Or Blowing Nose.\n
+        ☞ 🧼 Wash Your Hands Frequently.\n
+        ☞︎︎︎ 👬 Avoid Contact With Others.\n
+        ☞︎︎︎ 🍎 Wash Foods After buying it.", parse_mode=ParseMode.MARKDOWN)
+  
+@run_async
+def corona(bot: Bot, update: Update):
+    bot.sendChatAction(update.effective_chat.id, "typing") # Bot typing before send messages
+    message = update.effective_message
+    if message.reply_to_message:
+      message.reply_to_message.reply_text(random.choice(SFW_STRINGS))
+    else:
+      message.reply_text(random.choice(SFW_STRINGS))
+  
+  
 __help__ = """
  
- - /covid get worldwide corona status
+ - /covid - Get World Wide Corona Status
+ - /corona - Tips For Preventing You From Corona
 """
 
 __mod_name__ = 'Covid-19'
 
 COVID_HANDLER = CommandHandler("covid", covid, admin_ok=True)
+CRNA_HANDLER = DisableAbleCommandHandler("corona", corona)
+
 dispatcher.add_handler(COVID_HANDLER)
+dispatcher.add_handler(CRNA_HANDLER)
 
 
