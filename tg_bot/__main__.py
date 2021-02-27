cd2a850802ddb556e521bfbdcfc8dc870f873152
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-27 13:53:57 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -220,7 +220,7 @@ def help_button(bot: Bot, update: Update):
     try:
         if mod_match:
             module = mod_match.group(1)
-            text = "Here is the help for the *{}* module:\n".format(HELPABLE[module].__mod_name__) \
+            text = "Showing Help For Module\nㅤㅤㅤㅤㅤㅤ╚» *{}*\n".format(HELPABLE[module].__mod_name__) \
                    + HELPABLE[module].__help__
             query.message.reply_text(text=text,
                                      parse_mode=ParseMode.MARKDOWN,
