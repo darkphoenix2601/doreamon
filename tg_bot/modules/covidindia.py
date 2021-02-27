49ea399f252731b2a508247f183cf452273e1cc6
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-02-27 17:52:10 +0530

    Update covidindia.py

--- tg_bot/modules/covidindia.py
+++ tg_bot/modules/covidindia.py
@@ -1,55 +1,38 @@
-from telegram import ParseMode, Update, Bot, Chat
+from telegram import ParseMode, Update, Bot, Chat, MessageEntity
 from telegram.ext import CommandHandler, MessageHandler, BaseFilter, run_async
 
 from tg_bot import dispatcher
+from covid19_data import JHU
 
-import requests
+from requests import get
+from tg_bot.modules.disable import DisableAbleCommandHandler
 
 import json
 from urllib.request import urlopen
 
+@run_async
+def covidi(bot: Bot, update: Update):
+  update.effective_message.reply_text(
+      "*🦠 COVID-19 Stats 🦠:*\n\n"
+        "➥ *Total Confirmed* \nㅤㅤ╚» `" + str(JHU.Total.confirmed) + "`\n"
+        "➥ *Total Deaths* \nㅤㅤ╚» `" + str(JHU.Total.deaths) + "`\n"
+        "➥ *Total Recovered* \nㅤㅤ╚» `" + str(JHU.Total.recovered) +"`\n"
+        "➥ *Active Cases* \nㅤㅤ╚» `"+ str(JHU.India.cases) + "`\n\n"
+        "➥ *Tips*\n☞ 😷 Wear A Mask.\n ☞ 🧻 Use Tissue When Sneezing Or Blowing Nose.\n☞ 🧼 Wash Your Hands Frequently.\n☞︎︎︎ 👬 Avoid Contact With Others.\n☞︎︎︎ 🍎 Wash Foods Before Eating It.\n☞︎︎︎ 🛀 Maintain Good Hygiene", parse_mode=ParseMode.MARKDOWN)
+  
 
-def covindia(bot: Bot, update: Update):
-    message = update.effective_message
-    state = ''
-    confirmed = 0
-    deceased = 0
-    recovered = 0
-    state_input = ''.join([message.text.split(' ')[i] + ' ' for i in range(1, len(message.text.split(' ')))]).strip()
-    if state_input:
-        url_india = 'https://api.covid19india.org/data.json'
-        json_url = urlopen(url_india)
-        state_dict = json.loads(json_url.read())
-        for sdict in state_dict['statewise']:
-            if sdict['state'].lower() == state_input.lower():
-                confirmed = sdict['confirmed']
-                deceased = sdict['deaths']
-                recovered = sdict['recovered']
-                state = sdict['state']
-                break
-    
-    if state:
-        bot.send_message(
-            message.chat.id,
-            '`COVID-19 Tracker`\n*Number of confirmed cases in %s:* %s\n*Deceased:* %s\n*Recovered:* %s\n\n_Source:_ covid19india.org' % (state, confirmed, deceased, recovered),
-            parse_mode = ParseMode.MARKDOWN,
-            disable_web_page_preview = True
-        )
-    else:
-        bot.send_message(
-            message.chat.id,
-            'You need to specify a valid Indian state!',
-            parse_mode = ParseMode.MARKDOWN,
-            disable_web_page_preview = True
-        )
-
+  
 __help__ = """
  
- - /covindia <state>: Get real time COVID-19 stats for the input Indian state
+ ➥ /covid india - Get Corona Status Of India
+ 
 """
 
-__mod_name__ = 'Covid-19 Virus'
+__mod_name__ = 'Covid Tracker'
 
-COV_INDIA_HANDLER = CommandHandler('covindia', covindia)
+COVIDI_HANDLER = CommandHandler("covid india", covidi, admin_ok=True)
 
-dispatcher.add_handler(COV_INDIA_HANDLER)
+
+dispatcher.add_handler(COVIDI_HANDLER)
+
+
