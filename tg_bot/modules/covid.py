33f6726174a58fc5dee5d34cce76644577d5a727
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-27 13:17:56 +0530

    Update covid.py

--- tg_bot/modules/covid.py
+++ tg_bot/modules/covid.py
@@ -14,33 +14,14 @@ from urllib.request import urlopen
 
 @run_async
 def covid(bot: Bot, update: Update):
-    message = update.effective_message
-    device = message.text[len('/covid '):]
-    fetch = get(f'https://coronavirus-tracker-api.herokuapp.com/all')
-
-    if fetch.status_code == 200:
-        usr = fetch.json()
-        data = fetch.text
-        parsed = json.loads(data)
-        total_confirmed_global = parsed["latest"]["confirmed"]
-        total_deaths_global = parsed["latest"]["deaths"]
-        total_recovered_global = parsed["latest"]["recovered"]
-        active_cases_covid19 = total_confirmed_global - total_deaths_global - total_recovered_global
-        reply_text = ("*🦠 COVID-19 Stats 🦠:*\n\n"
+  update.effective_message.reply_text(
+      "*🦠 COVID-19 Stats 🦠:*\n\n"
         "➥ *Total Confirmed* \nㅤㅤ╚» `" + str(JHU.Total.confirmed) + "`\n"
         "➥ *Total Deaths* \nㅤㅤ╚» `" + str(JHU.Total.deaths) + "`\n"
         "➥ *Total Recovered* \nㅤㅤ╚» `" + str(JHU.Total.recovered) +"`\n"
         "➥ *Active Cases* \nㅤㅤ╚» `"+ str(JHU.India.cases) + "`\n\n"
         "➥ *Tips*\n☞ 😷 Wear A Mask.\n☞ 🧻 Use Tissue When Sneezing Or Blowing Nose.\n☞ 🧼 Wash Your Hands Frequently.\n☞︎︎︎ 👬 Avoid Contact With Others.\n☞︎︎︎ 🍎 Wash Foods After buying it.")
-        message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
-
-        return
-
-    elif fetch.status_code == 404:
-        reply_text = "The API is currently down."
-    message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
-
-
+        message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 __help__ = """
  
  - /covid get worldwide corona status
