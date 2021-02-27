dc9130992f4f5e7d0fbeee3e2b9b77fe5b6c607c
DIPANSHU <77918121+infotechbro@users.noreply.github.com>
2021-02-27 15:25:53 +0530

    Update afk.py

--- tg_bot/modules/afk.py
+++ tg_bot/modules/afk.py
@@ -20,7 +20,7 @@ def afk(bot: Bot, update: Update):
         reason = args[1]
 
     sql.set_afk(update.effective_user.id, reason)
-    update.effective_message.reply_text("{} Is Now Away!".format(update.effective_user.first_name))
+    update.effective_message.reply_text("*{}* Is Now Away!".format(update.effective_user.first_name), parse_mode=ParseMode.MARKDOWN)
 
 @run_async
 def no_longer_afk(bot: Bot, update: Update):
