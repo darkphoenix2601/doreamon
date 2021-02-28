21e9f50ffa56c493d99b7089e353167524fa5d66
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-03-01 00:08:08 +0530

    Update master_mod.py

--- tg_bot/modules/master_mod.py
+++ tg_bot/modules/master_mod.py
@@ -113,6 +113,9 @@ def info(bot: Bot, update: Update, args: List[str]):
 
     else:
         return
+ 
+    rep = message.reply.text(
+        "<code>Appraising Wait...</code>", parse_mode=ParseMode.HTML)
 
     text = (f"<b>➖➖➖➖➖➖➖➖➖➖\n「 User Info」 ℹ\n➖➖➖➖➖➖➖➖➖➖\n</b>\n"
             f"🆔️ ID ☞ <code>{user.id}</code>\n\n"
