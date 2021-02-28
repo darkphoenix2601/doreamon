d28ad8e5928fed26316c2c428fafb72af97641c3
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-02-28 11:44:57 +0530

    Update master_mod.py

--- tg_bot/modules/master_mod.py
+++ tg_bot/modules/master_mod.py
@@ -114,7 +114,7 @@ def info(bot: Bot, update: Update, args: List[str]):
     else:
         return
 
-    text = (f"<b>➖➖➖➖➖➖➖➖➖➖\n「 User Info」 ℹ\n➖➖➖➖➖➖➖➖➖➖:</b>\n\n"
+    text = (f"<b>➖➖➖➖➖➖➖➖➖➖\n╒═══「 User Info」 ℹ\n➖➖➖➖➖➖➖➖➖:</b>\n\n"
             f"🆔️ ID ☞ <code>{user.id}</code>\n"
             f"✔️ <b>First Name</b> ☞ <code>{html.escape(user.first_name)}</code>")
 
