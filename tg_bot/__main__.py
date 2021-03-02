2eae90f8acce6252e4105b5e9c46351ae5ab6e4a
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-03-02 17:22:47 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -185,7 +185,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                      ]]))
 
     else:
-        update.effective_message.reply_text("╔═════════════════╗\n「 ★᯾ вℓΛ¢к ℓєgєη∂ ᯾★ 」\n╚═════════════════╝",reply_markup=InlineKeyboardMarkup(
+        update.effective_message.reply_text("╔═════════════════╗\n 「 ★᯾ вℓΛ¢к ℓєgєη∂ ᯾★ 」\n╚═════════════════╝",reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="PM For Help",url="t.me/{}?start=help".format(bot.username))]]))
 
 
