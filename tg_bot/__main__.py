2c1f50fc52e0f2235120d1541efeaed320a5f869
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-09-13 08:23:11 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -161,7 +161,6 @@ def start(bot: Bot, update: Update, args: List[str]):
                 TECHNO_IMG,
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(
-                    reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
