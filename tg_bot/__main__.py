9cd6ab5f75e0423e43dce8ece3566205cec6e62a
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-09-13 08:09:58 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -131,8 +131,8 @@ def send_help(chat_id, text, keyboard=None):
 @run_async
 def alain(bot: Bot, update: Update):
     pprint(eval(str(update)))
-    update.effective_message.reply_text("📬*ʀᴇᴘᴏ ғᴏʀ ʙʟᴀᴄᴋ ʟᴇɢᴇɴᴅ ʙᴏᴛ ɪs* :- [ᴄʟɪᴄᴋ ʜᴇʀᴇ](t.me/JARVIS_SOURCE_CODE)", parse_mode=ParseMode.MARKDOWN)
-    update.effective_message.reply_text("📬*ʀᴇᴘᴏ ғᴏʀ ʙʟᴀᴄᴋ ʟᴇɢᴇɴᴅ ʙᴏᴛ ɪs* :- [ᴄʟɪᴄᴋ ʜᴇʀᴇ](t.me/JARVIS_SOURCE_CODE)_", parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_text("📬*ʀᴇᴘᴏ ғᴏʀ ʙʟᴀᴄᴋ ʟᴇɢᴇɴᴅ ʙᴏᴛ ɪs* :- [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/phoenix_music_suport)", parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_text("📬*ʀᴇᴘᴏ ғᴏʀ ʙʟᴀᴄᴋ ʟᴇɢᴇɴᴅ ʙᴏᴛ ɪs* :- [ᴄʟɪᴄᴋ ʜᴇʀᴇ](https://t.me/phoenix_music_suport)_", parse_mode=ParseMode.MARKDOWN)
     print(update.effective_message)
 
 @run_async
@@ -167,16 +167,19 @@ def start(bot: Bot, update: Update, args: List[str]):
                     ],
                      [
                          InlineKeyboardButton(
-                             text="👻 HELP 👻",
+                             text="HELP😀",
                             callback_data="help_back".format(bot.username)),
                          InlineKeyboardButton(
-                             text="UPDATES 🔀",
-                             url="https://t.me/tg_coders_updates")
+                             text="👻 SUPPORT🤩",
+                             url="https://t.me/phoenix_music_suport")
+                         InlineKeyboardButton(
+                             text="UPDATES😝",
+                             url="https://t.me/phoenix_music_new")
                      ],
                      [
                          InlineKeyboardButton(
-                             text="💻 MY CREATOR 💻",
-                             url="t.me/tg_x_coders")
+                             text="MY OWNER 👑",
+                             url="https://t.me/akshi_s_ashu")
                      ]]))
 
     else:
