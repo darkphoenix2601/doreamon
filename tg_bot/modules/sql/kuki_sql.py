2c7f81ebfbd8686f6239566dd9a5cc106e85857e
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-10-07 13:10:33 +0530

    Update kuki_sql.py

--- tg_bot/modules/sql/kuki_sql.py
+++ tg_bot/modules/sql/kuki_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from MoeZillaBot.modules.sql import BASE, SESSION
+from tg_bot.modules.sql import BASE, SESSION
 class KukiChats(BASE):
     __tablename__ = "kuki_chats"
     chat_id = Column(String(14), primary_key=True)
