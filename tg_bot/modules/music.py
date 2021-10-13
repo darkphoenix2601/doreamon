5cbc5405cc273a45cecae7600a7a7befa75e1277
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-10-13 21:21:24 +0530

    Create music.py

---
+++ tg_bot/modules/music.py
@@ -0,0 +1,8 @@
+mod_name = "◎Music"
+
+help = """⚜️ /play - (song name) - To search song from youtube and play directly
+⚜️ /pause - Pause Voice Chat Music.
+⚜️ /resume - Resume Voice Chat Music.
+⚜️ /skip - Skips the current Music Playing In Voice Chat.
+⚜️ /stop - Clears The Queue as well as ends Voice Chat Music.
+⚜️ /song (song name) - To search song and send song directly.
